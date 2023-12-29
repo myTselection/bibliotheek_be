d3a04cd68d5bb77f277ee8de4aec2a59769fc13a
myTselection <myTselection@gmail.com>
2023-12-29 13:22:45 +0100

    reload every +- 120min to limit slow reloads, forced reload can be triggered with service 'update'

--- custom_components/bibliotheek_be/sensor.py
+++ custom_components/bibliotheek_be/sensor.py
@@ -1,6 +1,7 @@
 import logging
 import asyncio
 from datetime import date, datetime, timedelta
+import random
 
 import homeassistant.helpers.config_validation as cv
 import voluptuous as vol
@@ -24,7 +25,7 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
     }
 )
 
-MIN_TIME_BETWEEN_UPDATES = timedelta(hours=1)
+MIN_TIME_BETWEEN_UPDATES = timedelta(minutes=120 + random.uniform(10, 20))
 
 
 async def dry_setup(hass, config_entry, async_add_devices):
